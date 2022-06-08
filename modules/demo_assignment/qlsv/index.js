728c734396caf44f9ec8931ac276e7582a9bbf2f
HoangTien339 <tiennh21@fpt.edu.vn>
2022-06-08 18:29:58 +0700

    Lesson 7:

--- modules/demo_assignment/qlsv/index.js
+++ modules/demo_assignment/qlsv/index.js
@@ -1,12 +1,20 @@
-function qlsvController($scope) {
+function qlsvController($scope, $http) {
 	$scope.list = [];
 	$scope.form_data = {
 		ho_ten: '',
 		ma_sv: '',
 		chuyen_nganh: 'udpm',
 		ngay_nhap_hoc: '',
+		email: '',
 		gioi_tinh: 1,
 	};
+
+	const url = "https://620b061fbeee430017f38481.mockapi.io/students";
+	$http.get(url)
+		.then(function (response) {
+			console.log(response)
+			$scope.list = response.data;
+		})
 
 	$scope.onBtnCreateClick = function (event) {
 		console.log($scope.form_data);
